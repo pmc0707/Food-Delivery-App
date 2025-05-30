8fc4e605e14560c3549b3eda36de3ac6109e5206
pmc0707 <pranshugoodboy@gmail.com>
2025-05-30 11:23:40 +0530

    deployment process

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb://localhost:27017/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://pmc0707:radha1907@cluster0.r7e7x.mongodb.net/food-del?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
    
 }
